cb5ede4026047825d54447b62afdbb32f9063992
Jason Adams <j.adams@f5.com>
2024-05-05 21:37:45 -0700

    Update module to Java 21

--- src/main/java/guru/springframework/pageview/PageViewServiceImpl.java
+++ src/main/java/guru/springframework/pageview/PageViewServiceImpl.java
@@ -2,13 +2,13 @@ package guru.springframework.pageview;
 
 import guru.springframework.config.RabbitConfig;
 import guru.springframework.model.events.PageViewEvent;
+import jakarta.xml.bind.JAXB;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.xml.bind.JAXB;
 import java.io.StringWriter;
 import java.io.Writer;
 
